5648f816bb03d1e551b502c4cf872b67c69a7660
faizanq <faizanqureshi0@gmail.com>
2018-09-29 08:03:05 +0530

    Implecit model binding and model scope

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -65,9 +65,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
+        // $user = User::findOrFail($id);
         return $this->SuccessResponse($user);
     }
 
@@ -89,9 +89,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-         $user = User::findOrFail($id);
+         // $user = User::findOrFail($id);
          $rules = [
                 'email'=>'email|unique:users,email,'.$user->id,
                 'password'=>'min:6|confirmed',
@@ -134,9 +134,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-         $user = User::findOrFail($id);
+         // $user = User::findOrFail($id);
          $user->delete();
          return $this->SuccessResponse($user);
         
